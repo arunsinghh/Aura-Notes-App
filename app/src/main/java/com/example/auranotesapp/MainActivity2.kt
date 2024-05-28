0e545def4841b6a4daa1a603fb7cf974e67c9700
arunsinghh <arunsinghh.dev@gmail.com>
2024-05-28 18:37:12 +0530

    Arun Singh

---
+++ app/src/main/java/com/example/auranotesapp/MainActivity2.kt
@@ -0,0 +1,22 @@
+package com.example.auranotesapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        },3000)
+    }
+}
