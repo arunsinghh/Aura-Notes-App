357f88c9ee81ab782504e079b45b3c7694e22a68
arunsinghh <arunsinghh.dev@gmail.com>
2024-05-28 14:14:49 +0530

    Arun Singh

---
+++ app/src/main/java/com/example/auranotesapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.auranotesapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.auranotesapp.database.NoteDatabase
+import com.example.auranotesapp.repository.NoteRepository
+import com.example.auranotesapp.viewmodel.NoteViewModel
+import com.example.auranotesapp.viewmodel.NoteViewModelFactory
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupViewModel()
+        
+    }
+
+    private fun setupViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
+    }
+}
