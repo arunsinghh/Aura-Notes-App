46ca7a516557e3a7f778102d5d134fd51e8bd6e4
arunsinghh <arunsinghh.dev@gmail.com>
2024-05-28 18:45:00 +0530

    Arun Singh

--- app/src/main/java/com/example/auranotesapp/MainActivity2.kt
+++ app/src/main/java/com/example/auranotesapp/MainActivity2.kt
@@ -17,6 +17,6 @@ class MainActivity2 : AppCompatActivity() {
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-        },3000)
+        },2000)
     }
 }
